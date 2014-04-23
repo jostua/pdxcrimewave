b53d816d3d35bc32b57f237ea662eb239bf05d50
Joshua Lynch <jlynch@mediadoc.com>
2014-04-23 00:04:26 -0700

    added labels?

--- Scripts/MapPrep.R
+++ Scripts/MapPrep.R
@@ -1,12 +1,13 @@
 library("rgdal")
 library("sp")
+library("maptools")
 ogrListLayers("Data/ShapeFiles/Neighborhoods_pdx.shp")
 shape <- readOGR("Data/ShapeFiles/Neighborhoods_pdx.shp", layer="Neighborhoods_pdx")
 plot(shape)
 
 #Set Colors for neighborhood based on reports of crime - B/W for testing
 shape$crimes = 1 #adds the attribute members
-shape$crimes[match(neighborhood.num$mapneighbor, shape$NAME)]=neighborhood.num$Count[match(neighborhood.num$mapneighbor, shape$NAME)] #will make new values at given countries
+shape$crimes[match(neighborhood.num$crimeneighbor, shape$NAME)]=neighborhood.num$Count[match(neighborhood.num$crimeneighbor, shape$NAME)] #will make new values at given countries
 shape$crimes[is.na(shape$crimes)] <- 0
 
 plot(shape, col=gray(1-(shape$crimes/max(shape$crimes)))) #adjust the colorramp
@@ -16,3 +17,17 @@ plot(shape)
 names(shape)
 
 shape$NAME
+
+gor <- readShapeSpatial("Data/ShapeFiles/Neighborhoods_pdx.shp")
+plot(gor)
+summary(gor)
+gor@data$NAME
+gor@data=merge(gor@data,neighborhood.num,by.x='NAME',by.y='crimeneighbor')
+nnames <- shape$NAME
+plot(gor,col=rgb(red = 0, green = 0, blue = 1-(gor@data$Count/max(gor@data$Count)), alpha=gor@data$Count/max(gor@data$Count)))
+text(getSpPPolygonsLabptSlots(gor), labels=nnames, cex=0.4)
+gor@data$NAME
+
+text(getSpPPolygonsLabptSlots(gor), labels=gor@data$OBJECTID, cex=0.4)
+shape$crimes
+gor@data$NAME[order(gor@data$NBO_)]
